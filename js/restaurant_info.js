b4c98bad97a34a6aad413b97a26ff9e59c84ac8f
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-09-20 22:28:10 -0400

    Updated map token key
    # using free token key from
    # [Mapbox](https://www.mapbox.com/)

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYnJvd25pYSIsImEiOiJjam1iZGp4cno1bjJhM3FwNHVjZGkwdGE5In0.8vnYqqKFfYDnH8gDZb92YA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
