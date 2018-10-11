f8dda50e450e8ae92b3b6aae7922f5aa4096ce46
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-10-11 14:37:24 -0400

    Updated the service worker register file.

--- js/register.js
+++ js/register.js
@@ -1,9 +1,11 @@
 /* Set up our service worker for the project */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(reg => {
-      console.log("Service worker registration successful: " + reg.scope);
-    })
-    .catch(error => {
-      console.log("Registration failed: " + error);
-    });
+  window.addEventListener('load', => {
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+        console.log("Service worker registration successful: " + reg.scope);
+      })
+      .catch(error => {
+        console.log("Registration failed: " + error);
+      });
+  });
 }
