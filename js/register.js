1d37629ac853dfd9a6df0b951f6c8e97a7893341
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-10-24 23:32:30 -0400

    Added a return variable to arrow function.

--- js/register.js
+++ js/register.js
@@ -1,11 +1,11 @@
 /* Set up our service worker for the project */
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
+  window.addEventListener('load', response => {
     navigator.serviceWorker.register('/sw.js').then(reg => {
         console.log("Service worker registration successful: " + reg.scope);
       })
       .catch(error => {
         console.log("Registration failed: " + error);
       });
-  });
+   });
 }
